bccc3a71c1a51288f0b5756491cd9d43818be1b9
ziad <ziadelsayedhamed@gmail.com>
2024-11-11 20:57:52 +0200

    change starting window desgin

--- ai_writing_frontend/src/pages/StartWindow.jsx
+++ ai_writing_frontend/src/pages/StartWindow.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function StartWindow() {
+  const navigate = useNavigate();
   return (
     <div className="flex h-screen">
     {/* Left Side - Welcome Message */}
-    <div className="w-1/2 flex flex-col justify-center items-center bg-blue-500 text-white p-10">
-      <h1 className="text-5xl font-bold mb-4">Welcome to WritingAssistant_Ai</h1>
+    <div className="w-1/2 flex flex-col justify-center items-center bg-gray-900 text-white p-10">
+      <h1 className="text-5xl font-bold mb-4">Welcome to Ai Writing Assestant</h1>
       <p className="text-lg mb-8 max-w-md text-center">
-        Discover innovative software solutions designed to streamline your business.
+        Discover Where Innovation Meets the Art of Writing designed to streamline your business.
       </p>
-      <button className="px-8 py-3 bg-white text-blue-500 rounded-full font-semibold hover:bg-gray-200 focus:outline-none transition">
+      <button onClick={()=> navigate('/login')} className="px-8 py-3 bg-white text-blue-500 rounded-full font-semibold hover:bg-gray-200 focus:outline-none transition">
         Get Started
       </button>
     </div>
@@ -17,7 +19,7 @@ function StartWindow() {
     {/* Right Side - Image/Graphic */}
     <div className="w-1/2 bg-gray-100 flex items-center justify-center">
       <img
-        src="https://via.placeholder.com/400" // Replace with your image URL
+        src="../public/img/technology.jpg" // Replace with your image URL
         alt="Welcome Graphic"
         className="w-3/4 h-auto rounded-lg shadow-lg"
       />
